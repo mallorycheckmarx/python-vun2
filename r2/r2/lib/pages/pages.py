3693cf2981fc48b2a17a081acb2e31254a5cecf0
english06 <mason.english@gmail.com>
2015-09-10 23:45:20 -0500

    Update pages.py

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -501,19 +501,7 @@ class Reddit(Templated):
             if is_moderator_with_perms('flair'):
                 buttons.append(NamedButton("flair", css_class="reddit-flair"))
 
-        if is_single_subreddit and is_moderator_with_perms('wiki'):
-            # append automod button if they have an AutoMod configuration
-            try:
-                WikiPage.get(c.site, "config/automoderator")
-                buttons.append(NamedButton(
-                    "automod",
-                    dest="../wiki/config/automoderator",
-                    css_class="reddit-automod",
-                ))
-            except tdb_cassandra.NotFound:
-                pass
-
-        if is_single_subreddit and is_moderator_with_perms('config'):
+        if is_single_subreddit and is_moderator_with_perms('wiki') or is_moderator_with_perms('config'):
             # append automod button if they have an AutoMod configuration
             try:
                 WikiPage.get(c.site, "config/automoderator")
