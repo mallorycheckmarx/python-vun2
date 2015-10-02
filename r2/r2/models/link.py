0537bcf09a2b0f27eb3fb3bb79fd0fa5c92b1b79
13steinj <13steinj@users.noreply.github.com>
2015-10-01 17:58:34 -0700

    Don't make things with NSFW in the title forced
    For this to be implemented it needs to be done in three steps, one starting to check for "NSFW" and set the over_18 attribute, then to make all current NSFW posts to have that attribute, then remove the miscellanious check.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -217,6 +217,12 @@ class Link(Thing, Printable):
             url = content
             selftext = cls._defaults["selftext"]
 
+        nsfwcheck = re.compile(r"\bnsf[wl]\b", re.I)
+        over_18 = False
+
+        if bool(nsfwcheck.search(title)):
+            over_18 = True
+
         l = cls(
             _ups=1,
             title=title,
@@ -230,6 +236,7 @@ class Link(Thing, Printable):
             ip=ip,
             comment_tree_version=cls._choose_comment_tree_version(),
             is_self=is_self,
+            over_18=over_18
         )
 
         l._commit()
