4aef0f54ef567ead8f6dc14a42e313375429741d
13steinj <13steinj@users.noreply.github.com>
2015-10-12 14:38:29 -0700

    Fix for nits
    Fixes nits

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -217,10 +217,8 @@ class Link(Thing, Printable):
             url = content
             selftext = cls._defaults["selftext"]
 
-        nsfwcheck = re.compile(r"\bnsf[wl]\b", re.I)
         over_18 = False
-
-        if bool(nsfwcheck.search(title)):
+        if cls._nsfw.search(title):
             over_18 = True
 
         l = cls(
@@ -236,7 +234,7 @@ class Link(Thing, Printable):
             ip=ip,
             comment_tree_version=cls._choose_comment_tree_version(),
             is_self=is_self,
-            over_18=over_18
+            over_18=over_18,
         )
 
         l._commit()
