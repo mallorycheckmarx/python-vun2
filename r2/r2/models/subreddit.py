4147900eb9345b8fe40e0852bccc73eb3e62918a
13steinj <13steinj@users.noreply.github.com>
2015-10-18 11:43:58 -0700

    Fix /r/all-<sub>/comments
    @Pokechu22 pointed out to check /r/all-<sub>/comments and /me/f/all/comments. It turns out that even without /comments being able to be sortable, they are broken due to the fact that they had not separate method to filter out comments.
    
    This commit resolves the above issue.

--- r2/r2/models/subreddit.py
+++ r2/r2/models/subreddit.py
@@ -1682,6 +1682,14 @@ class AllMinus(AllSR):
             q._filter(not_(Link.c.sr_id.in_(self.exclude_sr_ids)))
         return q
 
+    def get_all_comments(self, sort='new', time='all'):
+        from r2.models import Comment
+        from r2.lib.db.operators import not_
+        q = AllSR.get_all_comments(self, sort, time)
+        if c.user.gold and self.exclude_sr_ids:
+            q._filter(not_(Comment.c.sr_id.in_(self.exclude_sr_ids)))
+        return q
+
 
 class Filtered(object):
     unfiltered_path = None
