d902699fa8367a6ade699ff6ea8326cff9381bbf
rob <robert-v9@hotmail.com>
2015-10-19 19:27:28 +0100

    Clarify [removed] and [deleted].
    Make these terms more explicit in order to reduce user confusion.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -1478,9 +1478,9 @@ class Comment(Thing, Printable):
                     # If removed by an admin or moderator, distinguish that
                     # from being deleted by the user.
                     if item._spam:
-                        item.body = '[removed]'
+                        item.body = '[removed by moderator]'
                     else:
-                        item.body = '[deleted]'
+                        item.body = '[deleted by author]'
 
             if focal_comment == item._id36:
                 extra_css += " border"
