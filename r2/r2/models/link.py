6820b72f0f41a41aff41042679b64e6a5b9dae96
13steinj <13steinj@users.noreply.github.com>
2015-11-17 22:34:22 -0500

    Hotfix: Mod 2 non-mail mod modmail appears in the recipient inbox.
    Previously, if a mod sent a non mod a message via the subreddit (ex, so that a single thing can be discussed without that mod having full access to the modmail), it would not appear in the recipient inbox because the check did not care for or for not the mail perm, as reported [here](http://redd.it/3243tc). By changing it to an `is_moderator_with_perms` check, this issue is resolved.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -1999,7 +1999,7 @@ class Message(Thing, Printable):
         # also, only global admins can be message spammed.
         if not skip_inbox and to and (not m._spam or to.name in g.admins):
             # if "to" is not a sr moderator they need to be notified
-            if not sr_id or not sr.is_moderator(to):
+            if not sr_id or not sr.is_moderator_with_perms(to, 'mail'):
                 # Record the inbox relation, but don't give the user
                 # an orangered, if they PM themselves.
                 # Don't notify on PMs from blocked users, either
