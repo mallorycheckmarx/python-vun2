0d88a1d2570ee5c6755e4056fd76279aa2dc5249
13steinj <13steinj@users.noreply.github.com>
2016-02-04 23:26:04 -0500

    Add config based information for jsintervals
    Allows interval-events.js to interact with r.config and reddit's configuration files

--- r2/r2/public/static/js/interval-events.js
+++ r2/r2/public/static/js/interval-events.js
@@ -8,19 +8,27 @@
 
   _.extend(r.interval_events, {
     init: function() {
+      Function.prototype.bindArray = function(thisArg, args) {
+          args.unshift(thisArg);
+          return this.bind.apply(this, args)
+      }
       var objs = r.interval_events.intervalObjects;
       for (var obj = 0; obj < objs.length; obj++) {
         if (objs[obj].shouldRun()) {
-          objs[obj].as_interval = setInterval.apply(window, objs[obj].func.bind(objs[obj], objs[obj].funcparams), objs[obj].timer);
+          if (objs[obj].funcparams) {
+            objs[obj].as_interval = setInterval(objs[obj].func.bindArray(objs[obj], objs[obj].funcparams), objs[obj].timer);
+          } else {
+            objs[obj].as_interval = setInterval(objs[obj].func.bind(objs[obj]), objs[obj].timer);
+          }
         }
       }
     },
-    intervalObjects = [{
+    intervalObjects: [{
       name: 'modqueueCounter',
       func: function() {
         r.ajax({
           type: 'GET',
-          url: '/r/testmang/about/modqueue.json',
+          url: r.config.site_path.concat('/about/modqueue.json'),
           success: function(data) {
             var button = $('.reddit-modqueue');
             var counter = $('.special-modqueue-count');
@@ -46,7 +54,7 @@
       func: function() {
         r.ajax({
           type: 'GET',
-          url: '/r/testmang/about/unmoderated.json',
+          url: r.config.site_path.concat('/about/unmoderated.json'),
           success: function(data) {
             var button = $('.reddit-unmoderated');
             var counter = $('.special-unmoderated-count');
@@ -67,6 +75,6 @@
         return r.config.jsintervals.shouldRun[this.name] && $('.special-unmoderated-count');
       },
       timer: r.config.jsintervals.timers[this.name] || r.config.jsintervals.timers.default
-    }];
-  });
+    }
+  ]});
 }(r);
