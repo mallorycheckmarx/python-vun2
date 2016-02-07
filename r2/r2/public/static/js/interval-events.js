f1bbb98c8f15ec222006d37ca9930bdc61b92284
13steinj <13steinj@users.noreply.github.com>
2016-02-06 22:28:38 -0500

    Add intervalObjects example for future use

--- r2/r2/public/static/js/interval-events.js
+++ r2/r2/public/static/js/interval-events.js
@@ -2,6 +2,18 @@
   Set up actions to run on certain intervals of time
 
   requires r.config (base.js)
+
+  To add more events to this module to be run, add an
+  object to the intervalObjects array in this format:
+  {
+    name: <name to be used for the r.interval_events.intervalMapping>
+    func: <function to be run>
+    funcparams: <an array of params to be used when func is run (this seems useless, but can be used if the param can be variable based on multiple factors)> 
+    shouldRun: <function to see if the interval should be set, it's recommended to include a r.config.jsintervals.shouldRunp[this.name] check inside>
+    timer: <the timing to run on in seconds, recommended to check r.config.jsintervals.timers[this.name] and r.config.jsintervals.timers.default>
+  }
+  Other attributes can be added and set at will by the
+  author of the interval, for whatever use they wish.
   */
 !function(r) {
   r.interval_events = {};
@@ -9,17 +21,22 @@
   _.extend(r.interval_events, {
     init: function() {
       Function.prototype.bindArray = function(thisArg, args) {
+        if (typeof args === "undefined") {
+          return this.bind.apply(this, [thisArg]);
+        }
+        else if (args.constructor === Array) {
           args.unshift(thisArg);
-          return this.bind.apply(this, args)
+          return this.bind.apply(this, args);
+        } else {
+          throw TypeError('Function.prototype.bindArray: Arguments list has wrong type')
+        }
       }
+      r.interval_events.intervalMapping = {};
       var objs = r.interval_events.intervalObjects;
       for (var obj = 0; obj < objs.length; obj++) {
         if (objs[obj].shouldRun()) {
-          if (objs[obj].funcparams) {
-            objs[obj].as_interval = setInterval(objs[obj].func.bindArray(objs[obj], objs[obj].funcparams), objs[obj].timer);
-          } else {
-            objs[obj].as_interval = setInterval(objs[obj].func.bind(objs[obj]), objs[obj].timer);
-          }
+          objs[obj].interval_id = setInterval(objs[obj].func.bindArray(objs[obj], objs[obj].funcparams), objs[obj].timer);
+          r.interval_events.intervalMapping[objs[obj].name] = objs[obj].interval_id
         }
       }
     },
@@ -39,7 +56,7 @@
           error: function(errorObj) {
             if (errorObj.status == 403) {
               //mod lost permissions, unset interval
-              clearInterval(this.as_interval)
+              clearInterval(this.interval_id)
             }
           }
         });
@@ -65,7 +82,7 @@
           error: function(errorObj) {
             if (errorObj.status == 403) {
               //mod lost permissions, unset interval
-              clearInterval(this.as_interval)
+              clearInterval(this.interval_id)
             }
           }
         });
