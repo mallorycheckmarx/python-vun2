1552975bbf01e07925f7ba51599c380913a56526
13steinj <13steinj@users.noreply.github.com>
2016-03-11 16:50:54 -0500

    Reports: account for .reports_accepted
    Upon report acception, set .reports_accepted to True. Upon retreival, retreive if reports were accepted.

--- r2/r2/models/report.py
+++ r2/r2/models/report.py
@@ -118,6 +118,8 @@ class Report(MultiRelation('report',
             for thing in cls_things:
                 if thing.reported > 0:
                     thing.reported = 0
+                    if isinstance(thing, (Link, Comment)):
+                        thing.reports_accepted = True
                     thing._commit()
                     to_clear.append(thing)
 
@@ -126,7 +128,7 @@ class Report(MultiRelation('report',
     @classmethod
     def get_reports(cls, wrapped, max_user_reasons=20):
         """Get two lists of mod and user reports on the item."""
-        if (wrapped.reported > 0 and
+        if ((wrapped.reported > 0 or getattr(wrapped, "reports_accepted", False))and
                 (wrapped.can_ban or
                  getattr(wrapped, "promoted", None) and c.user_is_sponsor)):
             from r2.models import SRMember
@@ -176,7 +178,7 @@ class Report(MultiRelation('report',
     @classmethod
     def get_reasons(cls, wrapped):
         """Transition method in case API clients were already using this."""
-        if wrapped.can_ban and wrapped.reported > 0:
+        if wrapped.can_ban and (wrapped.reported > 0 or getattr(wrapped, "reports_accepted", False)):
             return [("This attribute is deprecated. Please use mod_reports "
                      "and user_reports instead.")]
         else:
