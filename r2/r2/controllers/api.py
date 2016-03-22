cb1501c34d6d6f220a4744ab5451961878df8c4a
13steinj <13steinj@users.noreply.github.com>
2016-03-22 16:08:21 -0400

    GET_morechildren: remove duplicate ids from children
    GET_morechildren is exploding down the line since the CommentBuilder doesn't add the comment properly to the child listing if attempting to be done the second time.

--- r2/r2/controllers/api.py
+++ r2/r2/controllers/api.py
@@ -3561,6 +3561,7 @@ class ApiController(RedditController):
                 return abort(403,'forbidden')
 
             if children:
+                children = list(set(children))
                 builder = CommentBuilder(link, CommentSortMenu.operator(sort),
                                          children=children,
                                          num=CHILD_FETCH_COUNT)
